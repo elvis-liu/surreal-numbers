d178fb3b2cf977ed1ae239a223983144fc02e71f
Elvis.Liu <elvisliu.cs@gmail.com>
2013-03-19 22:21:18 +1000

    Use midje to replace clojure.test

--- src/test/clojure/com/exmertec/elvis/surreal_numbers/basic_rules_test.clj
+++ src/test/clojure/com/exmertec/elvis/surreal_numbers/basic_rules_test.clj
@@ -1,38 +1,37 @@
 (ns com.exmertec.elvis.surreal_numbers.basic-rules-test
-  (:use clojure.test)
+  (:use midje.sweet)
   (:require [com.exmertec.elvis.surreal_numbers.basic-rules :as br]))
 
-(deftest identify-empty-set
-  (testing "empty-set? method can identify an empty set"
-    (is (br/empty-set? []))
-    (is (not (br/empty-set? [1])))
-    (is (not (br/empty-set? 3)))))
+(fact "how to identify empty set"
+  (br/empty-set? []) => true
+  (br/empty-set? [1]) => false
+  (br/empty-set? 3) => false)
 
-(deftest identify-number
-  (testing "valid-number? method can identify a valid surreal number"
-    (is (br/valid-number? {:left [], :right []}))
-    (is (br/valid-number? {:left 0, :right []}))
-    (is (br/valid-number? {:left [], :right 0}))
-    (is (br/valid-number? {:left 0, :right 0})))
-  (testing "valid-number? method can identify an invalid surreal number"
-    (is (not (br/valid-number? {})))
-    (is (not (br/valid-number? {:left []})))
-    (is (not (br/valid-number? {:right []})))
-    (is (not (br/valid-number? {:left[], :right [], :middle []})))
-    (is (not (br/valid-number? []))))
-  (testing "valid-number? method can accept a direct digit number as valid"
-    (is (br/valid-number? 3))
-    (is (br/valid-number? 0)))
-  (testing "valid-number? method can accept a more complex surreal number"
-    (is (br/valid-number? {:left {:left [], :right []}, :right []}))))
+(facts "how to identify valid numbers"
+  (fact "valid-number? method can identify a valid surreal number"
+    (br/valid-number? {:left [], :right []}) => true
+    (br/valid-number? {:left 0, :right []}) => true
+    (br/valid-number? {:left [], :right 0}) => true
+    (br/valid-number? {:left 0, :right 0}) => true)
+  (fact "valid-number? method can identify an invalid surreal number"
+    (br/valid-number? {}) => false
+    (br/valid-number? {:left []}) => false
+    (br/valid-number? {:right []}) => false
+    (br/valid-number? {:left[], :right [], :middle []}) => false
+    (br/valid-number? []) => false)
+  (fact "valid-number? method can accept a direct digit number as valid"
+    (br/valid-number? 3) => true
+    (br/valid-number? 0) => true)
+  (fact "valid-number? method can accept a more complex surreal number"
+    (br/valid-number? {:left {:left [], :right []}, :right []}) => true))
 
-(deftest create-number
-  (testing "create method can create a valid surreal number"
-    (is (br/valid-number? (br/create 0 1)))
-    (is (br/valid-number? (br/create [] [])))
-    (is (br/valid-number? (br/create [] 0)))
-    (is (br/valid-number? (br/create 0 [])))
-  (testing "create method throws exception for invalid components"
-    (is (thrown? Exception (br/create [0] 1)))
-    (is (thrown? Exception (br/create [0] [0])))
-    (is (thrown? Exception (br/create 1 [0]))))))
+(facts "how to create number"
+  (fact "create method can create a valid surreal number"
+    (br/valid-number? (br/create 0 1)) => true
+    (br/valid-number? (br/create [] [])) => true
+    (br/valid-number? (br/create [] 0)) => true
+    (br/valid-number? (br/create 0 [])) => true)
+  (fact "create method throws exception for invalid components"
+    (br/create [0] 1) => (throws Exception)
+    (br/create [0] [0]) => (throws Exception)
+    (br/create 1 [0]) => (throws Exception)))
